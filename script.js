55233641c10c2949072d15d608eebd0e7c144196
Avanish Ojha <85388688+Avanish08@users.noreply.github.com>
2025-06-25 09:41:52 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -61,3 +61,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
   typeLoop();
 });
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.getElementById("hamburger");
+  const navLinks = document.getElementById("nav-links");
+
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+});
+
