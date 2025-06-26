293d460057862c0d747e772b24f1ff4c28bab209
Avanish Ojha <85388688+Avanish08@users.noreply.github.com>
2025-06-26 16:41:03 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -69,4 +69,8 @@ document.addEventListener("DOMContentLoaded", () => {
     navLinks.classList.toggle("active");
   });
 });
-
+  window.addEventListener("load", function () {
+    const preloader = document.getElementById("preloader");
+    preloader.style.opacity = "0";
+    setTimeout(() => preloader.style.display = "none", 500);
+  });
